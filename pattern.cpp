3a351a05f68ec6cef2e604ac8e73430dd22362a2
virendra-1 <virendrakumarsharma8433@gmail.com>
2025-01-05 21:39:13 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printPattern() {
+    cout << "+*+" << endl;
+    cout << "++" << endl;
+    cout << "+*+" << endl;
+    cout << "++" << endl;
+    cout << "+" << endl;
+}
+
+int main() {
+    printPattern();
+    return 0;
+}
